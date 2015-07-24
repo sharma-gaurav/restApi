c6611b00fc079bb6774c5b6ad0c021113d28b1ef
sharma-gaurav <gauravs@intelligrape.com>
2015-07-24 12:51:52 +0530

    With Format

--- grails-app/controllers/com/intellimeet/CustomerController.groovy
+++ grails-app/controllers/com/intellimeet/CustomerController.groovy
@@ -1,5 +1,7 @@
 package com.intellimeet
 
+import grails.converters.JSON
+import grails.converters.XML
 import grails.rest.RestfulController
 
 class CustomerController extends RestfulController<Customer> {
@@ -7,5 +9,17 @@ class CustomerController extends RestfulController<Customer> {
 
     CustomerController() {
         super(Customer)
+    }
+
+    @Override
+    def index() {
+        def customers = Customer.list()
+
+        withFormat {
+            html customers: customers
+            json { render customers as JSON }
+            xml { render customers as XML }
+            '*' { render customers as JSON }
+        }
     }
 }
